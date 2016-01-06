b9d456ee95d00dd00b3dbd3adf9777838e59739a
Pavel Žák <just.paja@gmail.com>
2016-01-06 17:08:20 +0100

    Add test for backslashes

--- projects/test/scripts/run.sh
+++ projects/test/scripts/run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-. ${DIR_HOME}/inline/vars.sh
+. ${DIR_HOME}/lib/vars.sh
+. ${DIR_HOME}/lib/rewrite.sh
+
+src="${DIR_HOME}/inline/slashtest.txt"
+dest=$(mktemp)
 
 echo ${TEST_VAR}
+
+variable="variable"
+value="666"
+cp_rewrite ${src} ${dest} variable value
+
+cat ${dest}
+rm ${dest}
+
 echo "Exiting"
